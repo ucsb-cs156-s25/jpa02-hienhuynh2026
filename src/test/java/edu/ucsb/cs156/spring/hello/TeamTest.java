bcab3f7416870060becc9b1da2f5e81dd3f642d0
hienhuynh2026 <hienhuynh@ucsb.edu>
2025-04-03 22:13:03 -0700

    implementation ended with proper equal implementation

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -2,6 +2,8 @@ package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +29,65 @@ public class TeamTest {
     public void toString_returns_correct_string() {
         assertEquals("Team(name=test-team, members=[])", team.toString());
     }
+
+    @Test
+    public void equal_returns_true_for_equal_teams() {
+        Team team2 = new Team("test-team");
+        assertEquals(team, team2);
+    }
+
+    @Test
+    public void equal_returns_false_for_different_teams() {
+        Team team2 = new Team("test-team2");
+        assert(!team.equals(team2));
+    }
+
+    @Test
+    public void equal_returns_false_for_different_object() {
+        assert(!team.equals(new Object()));
+    }
+
+    @Test
+    public void equal_returns_true_for_same_object() {
+        assert(team.equals(team));
+    }
+    @Test
+    public void equal_returns_false_for_null() {
+        assert(!team.equals(null));
+    }
+    @Test
+    public void equal_returns_false_for_different_team() {
+        Team team2 = new Team("test-team");
+        team2.addMember("test-member");
+        assert(!team.equals(team2));
+    }
+    @Test
+    public void equal_returns_true_for_same_team() {
+        Team team2 = new Team("test-team");
+        assert(team.equals(team2));
+    }
+    @Test
+    public void equal_returns_false_for_different_team_name() {
+        Team team2 = new Team("test-team2");
+        assert(!team.equals(team2));
+    }
+    
+    @Test
+    public void hashCode_test():
+    
+    
+
+
+
+    
+
+
+
+    
+
+    
+
+    
     
 
 }
