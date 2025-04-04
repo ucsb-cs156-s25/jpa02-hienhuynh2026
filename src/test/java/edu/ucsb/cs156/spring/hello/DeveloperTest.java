314ee71d4b6ca934a02d002eda04aee228b327c8
hienhuynh2026 <hienhuynh@ucsb.edu>
2025-04-03 18:59:49 -0700

    hh - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -13,8 +13,11 @@ public class DeveloperTest {
     @Test
     public void testPrivateConstructor() throws Exception {
         // this hack is from https://www.timomeinen.de/2013/10/test-for-private-constructor-to-get-full-code-coverage/
+
         Constructor<Developer> constructor = Developer.class.getDeclaredConstructor();
+
         assertTrue(Modifier.isPrivate(constructor.getModifiers()),"Constructor is not private");
+
 
         constructor.setAccessible(true);
         constructor.newInstance();
@@ -23,10 +26,17 @@ public class DeveloperTest {
     @Test
     public void getName_returns_correct_name() {
        
-        assertEquals("Hien Huynh", Developer.getName());
+        assertEquals("Hien H.", Developer.getName());
     }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+       
+        assertEquals("hienhuynh2026", Developer.getGithubId());
+        
+    }
+
 
 }
